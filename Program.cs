c9ede16848574be14e5b90da09dcf5a6f906fba2
Pukste <ville_pukki@hotmail.com>
2019-08-29 13:34:23 +0300

    exceptioni ja offline puuttuu

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+
+namespace serverit
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+        }
+        
+    
+    }
+
+    class RealTimeCityBikeDataFetcher : ICityBikeDataFetcher
+    {
+        private int returnVal;
+        
+        public async Task<int> GetBikeCountInStation(string stationNAme)
+        {
+
+            using(var client = new HttpClient()){
+                HttpResponseMessage response = await client.GetAsync("http://api.digitransit.fi/routing/v1/routers/hsl/bike_rental");
+                using (HttpContent content = response.Content){
+                    string resp = await response.Content.ReadAsStringAsync();
+                    dynamic x = JsonConvert.DeserializeObject(resp);
+                    var stations = x.stations;
+                    var bikes = x.bikesAvailable;
+                    var name = x.name;
+                    foreach(var station in stations){
+                        if(name==stationNAme){
+                            returnVal= bikes;
+
+                        }
+
+                    }
+                }
+            }
+            
+            //poistaa virheen niin kauan kunnes saa json parserin toimimaan
+            return returnVal;
+
+
+
+        }
+
+        
+    }
+}
+
+public interface ICityBikeDataFetcher {
+    Task<int> GetBikeCountInStation(string stationNAme);
+}
