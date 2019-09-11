13ca920b0cca2676588d47ea4b4a8b2d173a2da5
Pukste <ville_pukki@hotmail.com>
2019-09-11 03:31:33 +0300

    Prettying up complete still 4/7

--- Assig2/Program.cs
+++ Assig2/Program.cs
@@ -6,23 +6,28 @@ using System.Linq;
 namespace Assig2{
     
     class Program{
-
+        
         static void Main(string[] args)
         {
+            Players players = MakeNewPlayers.players;
             MakeNewPlayers newPlayers = new MakeNewPlayers();
-            juttuja testaa = new juttuja();
             LiNQtest1 linqtest = new LiNQtest1();
             LiNQtest2 lingtest2 = new LiNQtest2();
             Console.WriteLine(args[0]);
             newPlayers.CreateGuids();
-            testaa.TestItems();
-            Player bob=new Player();
+            Player bob=players.playerlist[0];
+
+            Item item = bob.Items.returnhighersIlevel();
+            Console.WriteLine("Highest Item ID is {0} and its Item Level is {1}",item.Id,item.Level);
+
             Array getitems = linqtest.GetItems(bob);
             Array getitemswithlinq =linqtest.GetItemsWithLinq(bob);
             Console.WriteLine("GetItems lenght {0}", getitems.Length);
             Console.WriteLine("GetItemsWithLinq lenght {0}", getitemswithlinq.Length);
+
             Console.WriteLine("Id for GetFirst {0}",lingtest2.FirstItem(bob).Id);
             Console.WriteLine("ID for GetFirstWithLinq {0}",lingtest2.FirstItemWithLinq(bob).Id);
+            
             Console.WriteLine("meni jo");
         }
         
@@ -35,61 +40,39 @@ namespace Assig2{
     
 
     public class MakeNewPlayers{
-        Players players = new Players();
+        public static Players players = new Players();
         public void CreateGuids(){
-            for(int i =0; i < 1000; i++){
+            HashSet<Guid> hashset = new HashSet<Guid>();
+            for(int i =0; i < 1000000; i++){
                 Player p = new Player();
                 p.Id = Guid.NewGuid();
-                CheckIfIdInUse(p);
+                if(hashset.Contains(p.Id)){
+                    p.Id= Guid.NewGuid();
+                }
+                hashset.Add(p.Id);
                 p.Items = new List<Item>();
                 players.playerlist.Add(p);
+                
             }
-                void CheckIfIdInUse(Player x){
-                if(players.playerlist.Count>1){
-                    foreach(var p in players.playerlist){
-                        
-                        if(x.Id == p.Id){
-                            x.Id= Guid.NewGuid();
-                            CheckIfIdInUse(x);
-                        }
-                    }
-                }
-            }
-            Console.WriteLine(players.playerlist.Count);
-        }
-
-    }
-    public class juttuja{
-        Players players = new Players();
-        public Item TestItems(){
             Item first = new Item();
             first.Id = Guid.NewGuid();
             Item second = new Item();
             second.Id = Guid.NewGuid();
-            Player p = new Player();
             first.Level = 1;
             second.Level = 10;
-            p.Items = new List<Item>();
-            p.Items.Add(first);
-            p.Items.Add(second);
-                    
-            Item highest = p.Items.returnhighersIlevel();
-            Console.WriteLine("Highest Item ID {0} and Highest Item Level {1}",highest.Id,highest.Level);
-            return highest;
+            Player p1 = players.playerlist[0];
+            p1.Items = new List<Item>();
+            p1.Items.Add(first);
+            p1.Items.Add(second);
+            Console.WriteLine(players.playerlist.Count);
         }
+
     }
+
     public class LiNQtest1{
         
         
-        public Item[] GetItems(Player p){
-            p.Items = new List<Item>();
-            Item first = new Item();
-            Item second = new Item();
-            p.Items.Add(first);
-
-            p.Items.Add(second);
-
-            
+        public Item[] GetItems(Player p){         
 
             Item[] Playeritems = new Item[p.Items.Count];
             for(int i=0;i<p.Items.Count;i++){
@@ -97,48 +80,24 @@ namespace Assig2{
             }
             return Playeritems;
         }
-        public Item[] GetItemsWithLinq(Player p){
-            p.Items = new List<Item>();
-            Item first = new Item();
-            Item second = new Item();
-            p.Items.Add(first);
-            p.Items.Add(second);
-
-            
+        public Item[] GetItemsWithLinq(Player p){          
             return p.Items.ToArray();
         }
     }
 
     public class LiNQtest2{
         public Item FirstItem(Player p){
-            p.Items = new List<Item>();
-            Item first = new Item();
-            Item second = new Item();
-            p.Items.Add(first);
-            p.Items.Add(second);
-            p.Items[0].Id = Guid.NewGuid();
-            Console.WriteLine("ID of first item for without LINQ {0}", first.Id);
-            p.Items[1].Id = Guid.NewGuid();
-            Console.WriteLine("ID of second item for without LINQ {0}", second.Id);  
             if(p.Items.Count >0){
-                Item it = p.Items[0];
-                return it;
+                return p.Items[0];
             }
             else return null;           
         }
         public Item FirstItemWithLinq(Player p){
-            p.Items = new List<Item>();
-            Item first = new Item();
-            Item second = new Item();
-            p.Items.Add(first);
-            p.Items.Add(second);
-            p.Items[0].Id = Guid.NewGuid();
-            Console.WriteLine("ID of first item for with LINQ {0}", first.Id);
-            p.Items[1].Id = Guid.NewGuid();
-            Console.WriteLine("ID of second item for with LINQ {0}", second.Id);
             return p.Items.First();
         }
     }
+
+
 
     public interface IPlayer{
         int Score { get; set; }
